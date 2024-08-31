763a78cff79c9d64f1171c7c55fdaafba0792ceb
Fedoriaka <145745841+Fedoriaka@users.noreply.github.com>
2024-08-31 11:33:34 +0300

    10task

--- js/task-10.js
+++ js/task-10.js
@@ -3,28 +3,30 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-const buttonCreat = document.querySelector("[data-create]");
-const buttonDestroy = document.querySelector("[data-destroy]");
-const inputNumber = document.querySelector('input["type = number"]');
-const boxes = document.getElementById("boxes");
 
-buttonCreat.addEventListener("click", () => {
-  const number = Number(inputNumber.value);
+const boxes = document.getElementById("boxes");
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+const numberInput = document.querySelector('input[type="number"]');
+createButton.addEventListener("click", () => {
+  const number = Number(numberInput.value);
   createBoxes(number);
-  inputNumber.innerHTML = "";
+   numberInput.value = "";
+  
+});
+destroyButton.addEventListener("click", () => {
+  boxes.innerHTML = ""; 
 });
 function createBoxes(amount) {
   for (let i = 0; i < amount; i += 1) {
     const size = 30 + i * 10;
     const box = document.createElement("div");
-    box.style.weight = `${size}px`;
+
+    box.style.width = `${size}px`;
     box.style.height = `${size}px`;
+
     box.style.backgroundColor = getRandomHexColor();
 
     boxes.append(box);
   }
 }
-
-buttonDestroy.addEventListener("click", () => {
-  boxes.innerHTML = "";
-});
